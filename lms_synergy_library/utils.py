f7289482537f1a50b9dfe982db5e3d7759d9452b
kotorkovsciy <kotorkovsciy@gmail.com>
2023-05-03 11:25:33 +0300

    added methods for receiving soup notifications and the number of pages

--- lms_synergy_library/utils.py
+++ lms_synergy_library/utils.py
@@ -1,6 +1,7 @@
 from requests import Response, Session
 from bs4 import BeautifulSoup as bs
-from constants import URL_EDUCATION, URL_NEWS, URL_SCHEDULE, URLS_LANGUAGES
+from constants import URL_EDUCATION, URL_NEWS, URL_SCHEDULE, URLS_LANGUAGES, URL_NOTIFY
+from exceptions import PageNotExist
 
 
 class clean_data:
@@ -80,3 +81,67 @@ class SoupLms:
         response: Response = session.get(URL_EDUCATION, cookies=cookies, proxies=proxies)
 
         return bs(response.text, "html.parser")
+
+    @staticmethod
+    def get_soup_notify(session: Session, language: str, cookies: dict, proxies: dict, page: int = 1) -> bs:
+        """Returns soup notifications
+
+        :param session: Session
+        :param language: Language
+        :param cookies: Cookies
+        :param proxies: Proxies
+        :param page: Page
+
+        :type session: Session
+        :type language: str
+        :type cookies: dict
+        :type proxies: dict
+        :type page: int
+
+        :return: Soup notifications
+        :rtype: bs4.BeautifulSoup
+        """
+
+        if page < 1:
+            raise PageNotExist("Page does not exist: %s?page=%d&pageSize=10" % (URL_NOTIFY, page))
+
+        session.get(URLS_LANGUAGES[language], cookies=cookies, proxies=proxies)
+
+        response: Response = session.get(
+            "%s?page=%d&pageSize=10" % (URL_NOTIFY, page),
+            cookies=cookies,
+            proxies=proxies
+        )
+
+        return bs(response.text, "html.parser")
+
+    @classmethod
+    def get_amount_pages_notify(cls, session: Session, language: str, cookies: dict, proxies: dict) -> int:
+        """Returns amount pages notify
+
+        :param session: Session
+        :param language: Language
+        :param cookies: Cookies
+        :param proxies: Proxies
+
+        :type session: Session
+        :type language: str
+        :type cookies: dict
+        :type proxies: dict
+
+        :return: Amount pages notify
+        :rtype: int
+        """
+
+        soup: bs = cls.get_soup_notify(
+            session, language, cookies, proxies
+        )
+
+        paginator_links: bs = soup.select('.paginator a')
+
+        if paginator_links:
+            amount_pages: int = int(paginator_links[-2].text)
+        else:
+            amount_pages: int = 1
+
+        return amount_pages
