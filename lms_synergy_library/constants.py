d75f332db8a83f1292c261e753bf1a0aa5af36bc
kotorkovsciy <kotorkovsciy@gmail.com>
2023-05-03 13:06:35 +0300

    added link URL_NOTIFY_ARCHIVE

--- lms_synergy_library/constants.py
+++ lms_synergy_library/constants.py
@@ -7,6 +7,7 @@ URL_SCHEDULE: Final[str] = "%s/schedule/academ" % URL
 URL_NEWS: Final[str] = "%s/announce" % URL
 URL_EDUCATION: Final[str] = "%s/student/up" % URL
 URL_NOTIFY: Final[str] = "%s/student/notifications" % URL
+URL_NOTIFY_ARCHIVE: Final[str] = "%s/archive" % URL_NOTIFY
 URLS_LANGUAGES: Final[dict] = {
         "ru": "%s/user/lng/1" % URL,
         "en": "%s/user/lng/2" % URL,
